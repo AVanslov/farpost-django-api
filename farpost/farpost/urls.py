d80e18c73e8085ebc54a904e81f872e6ca69c752
AVanslov <a.buchelnikov99@gmail.com>
2024-07-22 13:49:01 +0200

    Set flake8

--- farpost/farpost/urls.py
+++ farpost/farpost/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.conf.urls import url
+from django.contrib import admin
 from django.urls import include, path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
